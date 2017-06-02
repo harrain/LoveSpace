838862bb22ead2f7d3c0ed00942a0a2641054c16
harrain <renzhicheng00@163.com>
2017-06-02 17:29:15 +0800

    闹钟activity初步界面搭建

---
+++ app/src/main/java/com/example/lovespace/home/alarm/AlarmActivity.java
@@ -0,0 +1,15 @@
+package com.example.lovespace.home.alarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lovespace.R;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
