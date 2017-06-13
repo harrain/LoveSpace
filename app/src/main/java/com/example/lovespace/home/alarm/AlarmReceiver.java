a6c118854e780c141d936c73c764ddced1f10310
harrain <renzhicheng00@163.com>
2017-06-13 18:49:07 +0800

    增加弹出对话框，可以关闭闹钟

--- app/src/main/java/com/example/lovespace/home/alarm/AlarmReceiver.java
+++ app/src/main/java/com/example/lovespace/home/alarm/AlarmReceiver.java
@@ -1,11 +1,14 @@
 package com.example.lovespace.home.alarm;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.lovespace.R;
@@ -15,16 +18,44 @@ import com.example.lovespace.R;
  */
 
 public class AlarmReceiver extends BroadcastReceiver {
+
+    private MediaPlayer player;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         ring(context);
-
         cancelAlarm(context,intent);
         Toast.makeText(context, "时间到了", Toast.LENGTH_LONG).show();
+        showDialog(context);
+    }
+
+    private void showDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("时间到了");
+        builder.setMessage("确定关闭闹钟吗？");
+        builder.setPositiveButton("关闭", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                player.stop();
+                player.release();
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+
+        //需要把对话框的类型设为TYPE_SYSTEM_ALERT，否则对话框无法在广播接收器里弹出
+        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        dialog.show();
     }
 
     private void ring(Context context){
-        MediaPlayer player = MediaPlayer.create(context, R.raw.piano);
+        player = MediaPlayer.create(context, R.raw.piano);
         player.setVolume(1f, 1f);
 
         player.start();
